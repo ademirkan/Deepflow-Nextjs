ee293994c5b716532fee2bc77b19b5f888c347b2
arifd <ademirkan@ucsd.edu>
2023-01-19 14:42:09 -0800

    removed '.json' from theme names

--- hooks/useTheme.tsx
+++ hooks/useTheme.tsx
@@ -2,21 +2,21 @@ import { useEffect } from "react";
 import { useThemeStore } from "../stores/useThemeStore";
 
 export const useTheme: () => [string, (theme: string) => void] = () => {
-    const [theme, setThemeByName] = useThemeStore((state) => [
+    const [themeName, setThemeByName] = useThemeStore((state) => [
         state.theme,
         state.setThemeByName,
     ]);
 
     useEffect(() => {
-        import(`../styles/themes/${theme}`).then((module) => {
+        import(`../styles/themes/${themeName}.json`).then((module) => {
             const theme = module.default;
             Object.keys(theme).forEach((key) => {
                 const value = theme[key];
                 document.documentElement.style.setProperty(key, value);
             });
         });
-        setThemeByName(theme);
-    }, [theme]);
+        setThemeByName(themeName);
+    }, [themeName]);
 
-    return [theme, setThemeByName];
+    return [themeName, setThemeByName];
 };
