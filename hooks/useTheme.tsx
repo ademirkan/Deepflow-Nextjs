72d732423ab92263d4c6f440cd633554e9c22660
arifd <ademirkan@ucsd.edu>
2023-01-21 22:45:23 -0800

    removed previous fix for SSR theme name, buggy

--- hooks/useTheme.tsx
+++ hooks/useTheme.tsx
@@ -2,25 +2,20 @@ import { useEffect, useState } from "react";
 import { useThemeStore } from "../stores/useThemeStore";
 
 export const useTheme: () => [string, (theme: string) => void] = () => {
-    // required to fix hydration error -- needs initial theme name to be same on
-    // client and server side render. Jank, but do not change!
-    const [theme, setTheme] = useState("undefined");
-
-    const [_themeName, setThemeByName] = useThemeStore((state) => [
+    const [themeName, setThemeByName] = useThemeStore((state) => [
         state.theme,
         state.setThemeByName,
     ]);
 
     useEffect(() => {
-        import(`../styles/themes/${_themeName}.json`).then((module) => {
+        import(`../styles/themes/${themeName}.json`).then((module) => {
             const theme = module.default;
             Object.keys(theme).forEach((key) => {
                 const value = theme[key];
                 document.documentElement.style.setProperty(key, value);
             });
-            setTheme(_themeName);
         });
-    }, [_themeName]);
+    }, [themeName]);
 
-    return [theme, setThemeByName];
+    return [themeName, setThemeByName];
 };
