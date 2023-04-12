b768f80bd1271cb93b6b1dbf07efa53691f9091a
arifd <ademirkan@ucsd.edu>
2023-04-11 22:52:51 -0700

    hardcoded firebase config, removed local env vars

--- firebase/clientApp.ts
+++ firebase/clientApp.ts
@@ -11,13 +11,14 @@ import { getDatabase, ref } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+    apiKey: "AIzaSyCpziWruRgMxSJtJqAGgB9X6KdfbeVaLCw",
+    authDomain: "deepflow-2ac86.firebaseapp.com",
+    databaseURL: "https://deepflow-2ac86-default-rtdb.firebaseio.com",
+    projectId: "deepflow-2ac86",
+    storageBucket: "deepflow-2ac86.appspot.com",
+    messagingSenderId: "395843510223",
+    appId: "1:395843510223:web:781735d9714d15a64c3cba",
+    measurementId: "G-DRZE3FHE69",
 };
 
 // Initialize Firebase
