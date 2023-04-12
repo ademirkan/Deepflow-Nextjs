5bd4acbc55b29d8d0b05ff8cedfd75d07f23d836
arifd <ademirkan@ucsd.edu>
2023-04-11 23:45:36 -0700

    revert hovered theme if not clicked

--- components/Modals/ThemeConfigModal.tsx
+++ components/Modals/ThemeConfigModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ReactModal from "react-modal";
 import _theme_index from "../../styles/themes/_theme_index.json";
 import styles from "../../styles/ThemeConfigModal.module.css";
@@ -7,22 +7,34 @@ import { useTheme } from "../../hooks/useTheme";
 export default function ThemeConfigModal(props: any) {
     const [searchInput, setSearchInput] = useState("");
     const [themeName, setThemeByName] = useTheme();
+    const [themeConfirmed, setThemeConfirmed] = useState(false);
+    const initialTheme = useRef(themeName);
 
     const filteredThemes = _theme_index.filter((themeName) =>
         themeName.toLowerCase().includes(searchInput.toLowerCase())
     );
 
+    const handleClose = () => {
+        if (!themeConfirmed) {
+            setThemeByName(initialTheme.current);
+        }
+        props.onRequestClose();
+    };
+
     return (
-        <ReactModal {...props}>
+        <ReactModal isOpen={props.isOpen} onRequestClose={handleClose}>
             <div className="flex flex-col gap-4 h-[70vh]">
-                <input
-                    type="text"
-                    className="border border-none bg-transparent rounded p-2 placeholder-text transition-colors focus:outline-none text-lg"
-                    placeholder="Type to search"
-                    value={searchInput}
-                    onChange={(e) => setSearchInput(e.target.value)}
-                    autoFocus
-                />
+                <div>
+                    <i className="fa-solid fa-magnifying-glass"></i>
+                    <input
+                        type="text"
+                        className="border border-none bg-transparent rounded p-2 placeholder-text transition-colors focus:outline-none text-lg"
+                        placeholder="Type to search"
+                        value={searchInput}
+                        onChange={(e) => setSearchInput(e.target.value)}
+                        autoFocus
+                    />
+                </div>
 
                 <div
                     className={`flex flex-col gap-2 overflow-y-auto flex-grow ${styles.customScrollbar}`}
@@ -32,6 +44,7 @@ export default function ThemeConfigModal(props: any) {
                             key={themeName}
                             className="leading-tight hover:bg-sub p-1 rounded"
                             onClick={() => {
+                                setThemeConfirmed(true);
                                 setThemeByName(themeName);
                                 setSearchInput("");
                                 props.onRequestClose();
